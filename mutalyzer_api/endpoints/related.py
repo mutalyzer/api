d69a63bd6d14ae201ece4cce5cf2d3b868c9c243
X.Liu <34545147+XLIU-hub@users.noreply.github.com>
2025-10-14 20:57:29 +0200

    Update API to use new retriever related module (#4)
    Authored-by: Xiaoyun Liu <x.liu@lumc.nl>

---
+++ mutalyzer_api/endpoints/related.py
@@ -0,0 +1,26 @@
+from flask import request
+from flask_restx import Namespace, Resource, reqparse
+from mutalyzer_retriever.related import get_related
+
+from .common import errors
+
+ns = Namespace("/")
+
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'locations',
+    type=str,
+    required=False,
+    default="0",
+    help='Semicolon-separated genomic location ranges (e.g., 100;300_400)'
+)
+@ns.route("/related_references/<string:accession>")
+class RelatedReferences(Resource):
+    @ns.expect(parser)
+    @errors
+    def get(self, accession):
+        args = parser.parse_args()
+        locations = args['locations']
+
+        return get_related(accession, locations)
+
