e302ae0364670bc0729253316876a95ca040f08f
Xiaoyun Liu <x.liu@lumc.nl>
2026-02-03 16:38:42 +0100

    Rename annotated_gens in equivalent hgvs.

--- mutalyzer_api/endpoints/all_annotated_genes.py
+++ mutalyzer_api/endpoints/all_annotated_genes.py
@@ -1,12 +1,23 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, reqparse
 from mutalyzer.equivalent import annotated_genes
 from .common import errors
 
 ns = Namespace("equivalent_hgvs")
 
-@ns.route("/<string:reference_id>/genes/all")
+_args = reqparse.RequestParser()
+
+_args.add_argument(
+    "reference_id",
+    type=str,
+    help="Reference ID.",
+    required=True,
+)
+
+@ns.route("/annotated_genes/")
 class AnnotatedGenes(Resource):
+    @ns.expect(_args)
     @errors
-    def get(self, reference_id):
+    def get(self):
         """List all annotated genes under a reference sequence ID."""
+        reference_id = _args.parse_args().get("reference_id")
         return annotated_genes(reference_id)
