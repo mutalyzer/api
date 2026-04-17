04141464f4e21ceadeecc9e12e9f7674cd82cb64
Xiaoyun Liu <x.liu@lumc.nl>
2026-04-17 13:01:09 +0200

    Register endpoints

--- mutalyzer_api/endpoints/api.py
+++ mutalyzer_api/endpoints/api.py
@@ -4,18 +4,25 @@ from flask import Blueprint, url_for
 from flask_restx import Api, apidoc, Namespace, Resource
 
 from mutalyzer.util import log_dir
-from .equivalent_description_on_overlap_transcript import ns as ns_equivalent_hgvs
-from .equivalent_genomic_description import ns as ns_equivalent_genomic_hgvs
-from .overlap_mane import ns as ns_overlap_mane
-from .overlap_genes import ns as ns_overlap_genes
-from .all_annotated_genes import ns as ns_annotated_genes
-from .annotated_transcripts import ns as ns_annotated_transcripts
-from .coordinate_to_g import ns as ns_coordinate_to_g
-from .g_to_coordinate import ns as ns_g_to_coordinate
-from .c_to_coordinate import ns as ns_c_to_coordinate
-from .coordinate_to_c import ns as ns_coordinate_to_c
-from .p_to_coordinate import ns as ns_p_to_coordinate
-from .coordinate_to_p import ns as ns_coordinate_to_p
+# from .genes import ns as ns_annotated_genes
+# from .transcripts import ns as ns_annotated_transcripts
+from .coordinate_to_genomic import ns as ns_coordinate_to_g
+from .coordinate_to_genomic_coding import ns as ns_coordinate_to_c_with_selector
+from .serialization import ns as ns_serialization
+from .coordinate_to_transcript_coding import ns as ns_coordinate_to_c_without_selector
+from .coordinate_to_genomic_noncoding import ns as ns_coordinate_to_n_selector
+from .coordinate_to_transcript_noncoding import ns as ns_coordinate_to_n_no_selector
+from .coordinate_to_protein import ns as ns_coordinate_to_protein
+from .coordinate_to_transcript_protein import ns as ns_coordinate_to_transcript_protein
+from .coordinate_to_genomic_protein import ns as ns_coordinate_to_genomic_protein
+from .genomic_to_coordinate import ns as ns_genomic_to_coordinate
+from .genomic_coding_to_transcript import ns as ns_genomic_coding_to_transcript
+from .trancript_coding_to_coordinate import ns as ns_trancript_coding_to_coordinate
+from .genomic_noncoding_to_trancript import ns as ns_genomic_noncoding_to_trancript
+from .transcript_noncoding_to_coordinate import ns as ns_transcript_noncoding_to_coordinate
+from .protein_to_coordinate import ns as ns_protein_to_coordinate
+from .genomic_protein_to_coordinate import ns as ns_genomic_protein_to_coordinate
+from .transcript_protein_to_coordinate import ns as ns_transcript_protein_to_coordinate
 
 from pkg_resources import get_distribution
 
@@ -62,14 +69,23 @@ class Version(Resource):
         return {"mutalyzer": get_distribution("mutalyzer").version, "api": API_VERSION}
 
 api.add_namespace(ns_version)
-api.add_namespace(ns_equivalent_hgvs)
-api.add_namespace(ns_equivalent_genomic_hgvs)
-api.add_namespace(ns_annotated_genes)
-api.add_namespace(ns_annotated_transcripts)
-api.add_namespace(ns_overlap_mane)
+# api.add_namespace(ns_annotated_genes)
+# api.add_namespace(ns_annotated_transcripts)
 api.add_namespace(ns_coordinate_to_g)
-api.add_namespace(ns_g_to_coordinate)
-api.add_namespace(ns_c_to_coordinate)
-api.add_namespace(ns_coordinate_to_c)
-api.add_namespace(ns_p_to_coordinate)
-api.add_namespace(ns_coordinate_to_p)
+api.add_namespace(ns_coordinate_to_c_with_selector)
+api.add_namespace(ns_serialization)
+api.add_namespace(ns_coordinate_to_c_without_selector)
+api.add_namespace(ns_coordinate_to_n_no_selector)
+api.add_namespace(ns_coordinate_to_n_selector)
+api.add_namespace(ns_coordinate_to_protein)
+api.add_namespace(ns_coordinate_to_transcript_protein)
+api.add_namespace(ns_coordinate_to_genomic_protein)
+
+api.add_namespace(ns_genomic_to_coordinate)
+api.add_namespace(ns_genomic_coding_to_transcript)
+api.add_namespace(ns_trancript_coding_to_coordinate)
+api.add_namespace(ns_genomic_noncoding_to_trancript)
+api.add_namespace(ns_transcript_noncoding_to_coordinate)
+api.add_namespace(ns_protein_to_coordinate)
+api.add_namespace(ns_genomic_protein_to_coordinate)
+api.add_namespace(ns_transcript_protein_to_coordinate)
